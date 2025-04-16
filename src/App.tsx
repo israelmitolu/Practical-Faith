eb74eddd34994b95b476973005874bbbd4e8fb5c
israelmitolu <israelmitolu@gmail.com>
2025-04-16 07:33:16 +0100

    Add @vercel/analytics package and integrate Analytics component in App.tsx for  tracking

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Preloader } from "@/components/Preloader";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
